edb6ec3255aa143cb8794dbe3fcd344e86fc41ca
mwallace37 <156851716+mwallace37@users.noreply.github.com>
2024-11-12 13:41:55 -0500

    Update remove_duplicates.java

--- programming files/remove_duplicates.java
+++ programming files/remove_duplicates.java
@@ -1,11 +1,15 @@
+//importing array/arraylist/list
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+//this is a class to remove duplicates
 public class remove_duplicates {
-
+    // this function returns a list of integers, given a list of integers and a list of elements to be removed
     public static List<Integer> removeDuplicates(List<Integer> arr, List<Integer> removedElements) {
+        //create list to return
         List<Integer> uniqueArr = new ArrayList<>();
+        //loop through elements and add them to uniqueArr if they aren't removed
         for (int i = 0; i < arr.size(); i++) {
             if (!uniqueArr.contains(arr.get(i))) {
                 uniqueArr.add(arr.get(i));
