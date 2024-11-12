c26258d0b949465dd9fc6ba9a2018fa34aa8fda0
AustinWindley <123011956+AustinWindley@users.noreply.github.com>
2024-11-12 13:39:39 -0500

    Commented datetime_convert.java

--- programming files/datetime_convert.java
+++ programming files/datetime_convert.java
@@ -1,13 +1,14 @@
+// Imports :)
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class datetime_convert {
     public static void main(String[] args) {
-        String dateStr = "2022-03-17 10:45:30";
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-        LocalDateTime dateObj = LocalDateTime.parse(dateStr, formatter);
-        String formattedDate = dateObj.format(DateTimeFormatter.ofPattern("MM/dd/yyyy HH:mm:ss"));
+        String dateStr = "2022-03-17 10:45:30"; // current date
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"); // formats date to readible format
+        LocalDateTime dateObj = LocalDateTime.parse(dateStr, formatter); // creates a date object to be used
+        String formattedDate = dateObj.format(DateTimeFormatter.ofPattern("MM/dd/yyyy HH:mm:ss")); // reformats date to month day year format
 
-        System.out.println(formattedDate);
+        System.out.println(formattedDate); // prints out date
     }
 }
